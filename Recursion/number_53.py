bbdefca6e1f0c8618a64eee0711d6b16f622e47a
rajjmalla10 <rajj.malla10@gmail.com>
2025-01-19 22:11:30 +0545

    New problems

---
+++ Recursion/number_53.py
@@ -0,0 +1,13 @@
+def fiboncaii(n):
+    if n <= 1:
+        return n
+    
+    table = [0] * (n+1)
+    table[1] = 1
+    for i in range(2, n+1):
+        table[i] = table[i-1] + table[i-2]
+    return table
+
+if __name__=="__main__":
+    print(fiboncaii(5))    
+    
