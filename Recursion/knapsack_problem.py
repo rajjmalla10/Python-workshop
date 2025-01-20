af8e253604220e4378ae012718495a8d44702f66
rajjmalla10 <rajj.malla10@gmail.com>
2025-01-21 04:34:06 +0545

    Completed knapsack I/O

--- Recursion/knapsack_problem.py
+++ Recursion/knapsack_problem.py
@@ -1,39 +1,43 @@
-def knapsack_prob(items):
-    max_weigth = 8
+def knapsack_prob(items,max_weigth):
+    
     table = [[0 for _ in range(max_weigth+1)] for _ in range(len(items)+1)]
     for row in range(1, len(items)+1):
         items_profit = items[row]['profit']
         items_weight = items[row]['weight']
         for col in range(max_weigth+1):
-            
-            
-            
-            if row == 0 or col == 0:
-                table[row][col] = 0
                 
-            elif col < items_weight:
+            if col >= items_weight:
                  table[row][col] = max(table[row-1] [col],
                                        table[row-1][col-items_weight] + items_profit )
             
                  
             else:
-                table[row][col] = table[row-1][col]       
-    
-    return table[len(items)+1][max_weigth]
-            
-           
+                table[row][col] = table[row-1][col]  
+                
+    selected_items = []                 
+    weig = max_weigth
+    for row in range(len(items),0,-1):
+        
+        if  table[row][weig] != table[row-1][weig]:
+            selected_items.append(row)
+            weig -= items[row]['weight']
                    
+    
+    return selected_items     
+    
+
 
 def main():
     items= {}
+    max_weigth = 8
     num_items = int(input("enter number of items: "))
     for i in range(1, num_items+1):
         items_weight = int(input(f"enter the weight of the item {i}: "))
         items_profit = int(input(f"enter the profit of the item {i}: "))
-        items[i] = {"profit" : items_weight , "weight": items_profit}
+        items[i] = {"profit" : items_profit , "weight": items_weight}
         
     print(items)
-    print(knapsack_prob(items))
+    print(knapsack_prob(items,max_weigth))
 
 if __name__=="__main__":
     main()
